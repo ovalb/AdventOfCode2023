787151f6ab434f21e26c9cb75be9d3bdc41fa63f
ovalb <giorgio.valbonesi@gmail.com>
2024-04-01 13:42:53 +0200

    day 10 p2

--- day_10/sol10.py
+++ day_10/sol10.py
@@ -1,5 +1,6 @@
 import os
 from collections import namedtuple
+from enum import Enum
 
 directory = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(directory, "input")
@@ -53,7 +54,7 @@ def jump(pos: Pos, dir: str) -> Pos:
 
 
 def go_forward(elem: Elem) -> Elem:
-    ch = grid[elem[0].x][elem[0].y]
+    ch = grid[elem.p.x][elem.p.y]
     nextdirset = set(outdirs[ch]) - set(flipdir(elem.d))
     nextdir = nextdirset.pop()
     return Elem(jump(elem.p, nextdir), nextdir)
@@ -80,5 +81,52 @@ def p1():
         arr2.append(next2)
 
 
-sol = p1()
-print("sol is:", sol)
+# sol = p1()
+# print("sol is:", sol)
+
+
+def reverses_number(i, y, wall):
+    rev = 0
+    for x in range(y + 1, len(grid[i])):
+        if Pos(i, x) in wall and grid[i][x] in {"|", "L", "J"}:
+            rev += 1
+    return rev
+
+
+# using raytracing
+def p2():
+    S = findS()
+    all_neighbors = [Elem(jump(S, dir), dir) for dir in ["N", "W", "E", "S"]]
+    valid_neighbors = [n for n in all_neighbors if valid_elem(n)]
+    wall = set()
+    cur = valid_neighbors[0]
+    wall.add(cur.p)
+    while cur.p != S:
+        cur = go_forward(cur)
+        wall.add(cur.p)
+
+    # understand what is S
+    neighdir = (valid_neighbors[0].d, valid_neighbors[1].d)
+    nd = neighdir if neighdir in outdirs.values() else (neighdir[1], neighdir[0])
+    s_char = list(outdirs.keys())[list(outdirs.values()).index(nd)]
+    print(f"s_char is {s_char}")
+
+    # replace S with real pipe
+    for i, row in enumerate(grid):
+        grid[i] = row.replace("S", s_char)
+
+    result = 0
+    for i, row in enumerate(grid):
+        for y, _ in enumerate(row):
+            if Pos(i, y) not in wall and reverses_number(i, y, wall) % 2 == 1:
+                print(f"({i},{y})")
+                result += 1
+
+    return result
+
+
+sol2 = p2()
+print(f"sol2 is {sol2}")
+
+# https://en.wikipedia.org/wiki/Pick%27s_theorem
+# https://en.wikipedia.org/wiki/Shoelace_formula
