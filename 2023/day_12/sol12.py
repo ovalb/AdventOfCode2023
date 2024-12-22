e0e8dd41468c0865c5dc83b6b4e535801bdb037d
ovalb <giorgio.valbonesi@gmail.com>
2024-12-22 03:25:54 +0100

    move old stuff to 2023 folder

---
+++ 2023/day_12/sol12.py
@@ -0,0 +1,16 @@
+lines = open('sample').read().split('\n')
+
+# P = n**2
+
+for line in lines:
+    [pipes, arg] = line.split(' ')
+
+# '#' delimits the beginning of a sequence
+# a substring matched OR '.' delimits the end of a sequence
+
+
+#.#.###
+
+#.# = 1.1
+
+
